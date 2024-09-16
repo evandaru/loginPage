266466f2157cb1e71c8fac3e6035989fdc96fea1
uzaaannn <108566614+evandaru@users.noreply.github.com>
2024-09-16 13:30:24 +0700

    a

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://login-page-liard-chi.vercel.app'; // Gunakan variabel environment dari Vite
+const API_URL = 'https://login-page-liard-chi.vercel.app';
 
 export const login = async (email, password) => {
     const response = await fetch(`${API_URL}/login`, {
@@ -30,28 +30,4 @@ export const signup = async (name, email, password) => {
     }
 
     return await response.json();
-};
-
-// Fungsi untuk menyimpan token dan waktu login ke localStorage
-export const setSession = (name, token) => {
-    const loginTime = new Date().getTime(); // Mendapatkan waktu login dalam milidetik
-    localStorage.setItem('user', JSON.stringify({ name, token, loginTime }));
-};
-
-// Fungsi untuk memeriksa apakah sesi masih valid
-export const isSessionExpired = () => {
-    const session = JSON.parse(localStorage.getItem('user'));
-    if (!session) return true;
-
-    const currentTime = new Date().getTime();
-    const timeDiff = currentTime - session.loginTime;
-    // const fiveMinutes = 10 * 1000; // 5 menit dalam milidetik
-    const fiveMinutes = 5 * 60 * 1000; // 5 menit dalam milidetik
-
-    return timeDiff > fiveMinutes; // Jika lebih dari 5 menit, sesi dianggap expired
-};
-
-// Fungsi untuk menghapus sesi
-export const clearSession = () => {
-    localStorage.removeItem('user');
 };
